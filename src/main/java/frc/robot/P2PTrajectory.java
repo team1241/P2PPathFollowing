c80b77f55f7cf68b0db1d50d1b8209bad917c52d
AndyDai602 <andydai602@gmail.com>
2023-08-21 12:07:10 -0400

    moved waypoint class in trajectory

--- src/main/java/frc/robot/P2PTrajectory.java
+++ src/main/java/frc/robot/P2PTrajectory.java
@@ -1,6 +1,26 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+
 public class P2PTrajectory {
+
+    public class P2PWaypoint {
+        private Pose2d pose;
+        private double endRadius;
+
+        public P2PWaypoint() {
+
+        }
+
+        public void setEndRadius(double endRadius) {
+            this.endRadius = endRadius;
+        }
+
+        public void setPose(Pose2d pose) {
+            this.pose = pose;
+        }
+    }
+
     private P2PWaypoint[] waypoints;
     private int waypointIndex;
 
