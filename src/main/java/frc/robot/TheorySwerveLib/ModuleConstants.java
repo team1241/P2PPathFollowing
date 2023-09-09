6c6d0925ae5d7f4546d78791949c3751f5547e6d
marwazaryab <118633267+marwazaryab@users.noreply.github.com>
2023-09-09 19:40:27 -0400

    implemented our swerve library

---
+++ src/main/java/frc/robot/TheorySwerveLib/ModuleConstants.java
@@ -0,0 +1,15 @@
+package frc.robot.TheorySwerveLib;
+
+public final class ModuleConstants {
+    public static double TURN_RATIO = 0;
+    public static double WHEEL_RATIO = 0;
+
+    public static double TURN_TICKS_PER_REV = 0;
+    public static double WHEEL_TICKS_PER_REV = 0;
+    public static double WHEEL_DIAMETER = 0;
+
+    public static double WHEEL_VELOCITY_MEASURE_PERIOD_MS = 0;
+
+    public static double WHEEL_MAX_SPEED = 0;
+
+}
